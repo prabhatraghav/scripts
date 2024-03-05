3fa83e10ab272d47bda4449989b91eec952c4489
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-05 17:57:04 +0530

    Update k8s-nodes.sh

--- k8s-nodes.sh
+++ k8s-nodes.sh
@@ -1,7 +1,7 @@
 ## Install Docker
 
 
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/devops-labs-scripts/master/scripts/installDocker.sh -P /tmp
 sudo chmod 755 /tmp/installDocker.sh
 sudo bash /tmp/installDocker.sh
 sudo systemctl restart docker.service
@@ -10,7 +10,7 @@ sudo systemctl restart docker.service
 ## Install CRI-Docker
 
 
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installCRIDockerd.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/devops-labs-scripts/master/scripts/installCRIDockerd.sh -P /tmp
 
 
 sudo chmod 755 /tmp/installCRIDockerd.sh
@@ -21,6 +21,6 @@ sudo systemctl restart cri-docker.service
 ## Install kuubernetes
 
 
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S-v1-23.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/devops-labs-scripts/master/scripts/installK8S-v1-23.sh -P /tmp
 sudo chmod 755 /tmp/installK8S-v1-23.sh
 sudo bash /tmp/installK8S-v1-23.sh
