ed38cabef16c7a54d7c56563b3a786e38e1a0cf8
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 02:53:09 +0530

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,32 +1,48 @@
-## Install Docker
+## Install and enable Docker engine
 sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/docker-engine.sh -P /tmp
 sudo chmod 755 /tmp/docker-engine.sh
 sudo bash /tmp/docker-engine.sh
 sudo systemctl restart docker.service
 
 
-## Install CRI-Docker
+## Install and enable CRI-Docker CRI (Container Runtime Interface)
 sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/cri-dockerd.sh -P /tmp
 sudo chmod 755 /tmp/cri-dockerd.sh
 sudo bash /tmp/cri-dockerd.sh
 sudo systemctl restart cri-docker.service
 
 
-## Install kubernetes
+## Install kubernetes (Kubeadm, Kubelet, Kubectl)
 sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/install-k8s-v129.sh -P /tmp
 sudo chmod 755 /tmp/install-k8s-v129.sh
 sudo bash /tmp/install-k8s-v129.sh
 
 ## Initialize kubernetes Master Node
- 
 sudo kubeadm init --cri-socket unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=all
 
+## Create a single-host Kubernetes cluster
+# sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+
+## To start using your cluster, you need to run the following as a regular user
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+## Install anyone networking driver -- Weave/flannel/canal/calico/cni etc... 
 
-## install networking driver -- Weave/flannel/canal/calico etc... 
+## Installing Calico (https://docs.tigera.io/calico/latest/getting-started/kubernetes/quickstart)
+# Install the Tigera Calico operator and custom resource definitions
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml
 
-## below installs calico networking driver   
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml
+#Install Calico by creating the necessary custom resource
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/custom-resources.yaml
+
+## Confirm that all of the pods are running (Wait until each pod has the STATUS of Running)
+watch kubectl get pods -n calico-system
+
+# Remove the taints on the control plane
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# Confirm that you now have a node in your cluster
+kubectl get nodes -o wide
