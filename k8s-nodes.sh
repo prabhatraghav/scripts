3033e6a14b7282f2ac9734e10ed0a00591d579f4
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 00:04:24 +0530

    Update k8s-nodes.sh

--- k8s-nodes.sh
+++ k8s-nodes.sh
@@ -13,8 +13,6 @@ sudo systemctl restart cri-docker.service
 
 
 ## Install kubernetes
-
-
 sudo wget https://raw.githubusercontent.com/prabhatraghav/devops-labs-scripts/master/scripts/installK8S-v1-23.sh -P /tmp
 sudo chmod 755 /tmp/installK8S-v1-23.sh
 sudo bash /tmp/installK8S-v1-23.sh
