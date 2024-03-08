9fecdd893a597f375afba7c0fc9e0c3918393837
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 02:36:54 +0530

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,21 +1,21 @@
 ## Install Docker
 sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/docker-engine.sh -P /tmp
-sudo chmod 755 /tmp/installDocker.sh
-sudo bash /tmp/installDocker.sh
+sudo chmod 755 /tmp/docker-engine.sh
+sudo bash /tmp/docker-engine.sh
 sudo systemctl restart docker.service
 
 
 ## Install CRI-Docker
 sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/cri-dockerd.sh -P /tmp
-sudo chmod 755 /tmp/installCRIDockerd.sh
-sudo bash /tmp/installCRIDockerd.sh
+sudo chmod 755 /tmp/cri-dockerd.sh
+sudo bash /tmp/cri-dockerd.sh
 sudo systemctl restart cri-docker.service
 
 
 ## Install kubernetes
-sudo wget https://raw.githubusercontent.com/prabhatraghav/devops-labs-scripts/master/scripts/installK8S-v1-28.sh -P /tmp
-sudo chmod 755 /tmp/installK8S-v1-23.sh
-sudo bash /tmp/installK8S-v1-23.sh
+sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/install-k8s-v129.sh -P /tmp
+sudo chmod 755 /tmp/install-k8s-v129.sh
+sudo bash /tmp/install-k8s-v129.sh
 
 ## Initialize kubernetes Master Node
  
