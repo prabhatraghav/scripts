081986bd97783d9eb189730bed8ccc30cb598202
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 00:35:07 +0530

    Update docker-engine.sh

--- docker-engine.sh
+++ docker-engine.sh
@@ -1,1 +1,3 @@
-
+# Before you can install Docker Engine, you need to uninstall any conflicting packages
+# command to uninstall all conflicting packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
