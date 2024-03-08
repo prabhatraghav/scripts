58fc4c981827b6c60d14a45b1ce606a9a10f5e71
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 00:06:22 +0530

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -16,3 +16,17 @@ sudo systemctl restart cri-docker.service
 sudo wget https://raw.githubusercontent.com/prabhatraghav/devops-labs-scripts/master/scripts/installK8S-v1-23.sh -P /tmp
 sudo chmod 755 /tmp/installK8S-v1-23.sh
 sudo bash /tmp/installK8S-v1-23.sh
+
+## Initialize kubernetes Master Node
+ 
+sudo kubeadm init --cri-socket unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=all
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+## install networking driver -- Weave/flannel/canal/calico etc... 
+
+## below installs calico networking driver   
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml
