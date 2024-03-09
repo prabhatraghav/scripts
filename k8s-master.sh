f0e3a616319963c9df8d4ad35226ef1eb4f3ce56
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 17:03:58 +0530

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -34,12 +34,16 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 ## Install anyone networking driver -- Weave/flannel/canal/calico/cni etc... 
 
 ## Installing Calico (https://docs.tigera.io/calico/latest/getting-started/kubernetes/quickstart)
+
 # Install the Tigera Calico operator and custom resource definitions
 #sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml
 
 #Install Calico by creating the necessary custom resource
 #sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/custom-resources.yaml
+
+#Install Calico with default settings
 sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml
+
 ## Confirm that all of the pods are running (Wait until each pod has the STATUS of Running)
 # watch kubectl get pods -n calico-system
 
@@ -54,4 +58,4 @@ sudo kubectl get nodes -o wide
 ## (Please integrate the CNI socket by pasting it within the generated token just after the "<master-node-IP>:6443 --cri-socker unix:///var/run/cri-dockerd.sock --token"):
 #sudo kubeadm token create --print-join-command
 echo "Please copy the below command as it is and paste it to all the worker-nodes to join the k8s-cluster:"
-echo "$(sudo kubeadm token create --print-join-command) --cri-socker unix:///var/run/cri-dockerd.sock"
+echo "sudo $(sudo kubeadm token create --print-join-command) --cri-socker unix:///var/run/cri-dockerd.sock"
