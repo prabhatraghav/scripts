52cf577987d328bcf18a192451c404d0217a8b19
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 18:06:57 +0530

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -55,8 +55,8 @@ sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.
 sudo kubectl get nodes -o wide
 
 ## To generate token in master-node and copy to all the worker-nodes
-## (Please integrate the CNI socket by pasting it within the generated token just after the "<master-node-IP>:6443 --cri-socker unix:///var/run/cri-dockerd.sock --token"):
+## (Please integrate the CNI socket by pasting it within the generated token just after the "<master-node-IP>:6443 --cri-socket unix:///var/run/cri-dockerd.sock --token"):
 #sudo kubeadm token create --print-join-command
 echo "Please copy and paste the below command to all the worker-nodes to join the k8s-cluster (use it with "sudo" if u r not a root user):"
-sudo kubeadm token create --print-join-command | sed "s#:6443#& --cri-socker unix:///var/run/cri-dockerd.sock#"
-## echo "sudo $(sudo kubeadm token create --print-join-command) --cri-socker unix:///var/run/cri-dockerd.sock"
+sudo kubeadm token create --print-join-command | sed "s#:6443#& --cri-socket unix:///var/run/cri-dockerd.sock#"
+## echo "sudo $(sudo kubeadm token create --print-join-command) --cri-socket unix:///var/run/cri-dockerd.sock"
