5dab661f8c6100bc7acec08df3c650422c7771f6
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-03-09 21:47:34 +0530

    Update k8s-master.sh

--- k8s/k8s-master.sh
+++ k8s/k8s-master.sh
@@ -1,5 +1,5 @@
 ## Install and enable Docker engine
-sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/docker-engine.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/scripts/main/k8s/latest-k8s-129/docker-engine.sh -P /tmp
 sudo chmod 755 /tmp/docker-engine.sh
 sudo bash /tmp/docker-engine.sh
 sudo systemctl enable docker.service
@@ -7,19 +7,19 @@ sudo systemctl restart docker.service
 
 
 ## Install and enable CRI-Docker CRI (Container Runtime Interface)
-sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/cri-dockerd.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/scripts/main/k8s/latest-k8s-129/cri-dockerd.sh -P /tmp
 sudo chmod 755 /tmp/cri-dockerd.sh
 sudo bash /tmp/cri-dockerd.sh
 sudo systemctl enable cri-docker.service
 sudo systemctl restart cri-docker.service
 
 ## Install kubernetes (Kubeadm, Kubelet, Kubectl)
-sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/install-k8s-v129.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/scripts/main/k8s/latest-k8s-129/install-k8s-v129.sh -P /tmp
 sudo chmod 755 /tmp/install-k8s-v129.sh
 sudo bash /tmp/install-k8s-v129.sh
 
 ## Initialise the k8s-cluster on master-node
-sudo wget https://raw.githubusercontent.com/prabhatraghav/Deployment-script/main/latest-k8s-129/start-k8s-cluster.sh -P /tmp
+sudo wget https://raw.githubusercontent.com/prabhatraghav/scripts/main/k8s/latest-k8s-129/start-k8s-cluster.sh -P /tmp
 sudo chmod 755 /tmp/start-k8s-cluster.sh
 sudo bash /tmp/start-k8s-cluster.sh
 
