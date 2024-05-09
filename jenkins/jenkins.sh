5807dcbef874a9089d6224757e8681b102337fff
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-05-09 13:07:09 +0530

    Update jenkins.sh

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,18 +1,21 @@
+# For UBUNTU 22.04 - EC2-INSTANCE: Atleast minimum 2GB RAM + 2vCPUs - will be required to work Jenkins smoothly.
+# sudo wget https://raw.githubusercontent.com/prabhatraghav/scripts/edit/master/jenkins/jenkins.sh
+# sudo chmod +x jenkins.sh
+# sudo sh jenkins.sh
+
 #!/bin/bash
-# USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
 sudo apt update -y
-sudo apt install openjdk-11-jdk -y
+sudo apt install openjdk-21-jdk -y
 sudo apt update -y
-sudo apt install openjdk-8-jdk -y
 sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo apt update -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install jenkins -y
-service jenkins start
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 cat /var/lib/jenkins/secrets/initialAdminPassword
-#chmod 777 jenkins.sh
-#./jenkins.sh
