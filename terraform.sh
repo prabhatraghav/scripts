052c5663ef8bf796fb7a2c8af191cc91d717f4ae
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-05-16 11:59:44 +0530

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,31 @@
+# For UBUNTU (tested on Ubuntu-22.04-LTS/Ubuntu-24.04-LTS)
+# sudo wget https://raw.githubusercontent.com/prabhatraghav/scripts/master/terraform.sh
+# sudo chmod +x terraform.sh
+# sudo sh terraform.sh
+
+#!/bin/bash
+sudo apt-get update -y
+
+# Installing dependencies
+sudo apt-get install gnupg software-properties-common -y
+
+# Installing the HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Verifing the Key fingerprints
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+# Adding the official HashiCorp repository to the APT package manager
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Updating the package information from HashiCorp to the APT package manager
+sudo apt update -y
+
+# Installing Terraform from the new repository.
+sudo apt-get install terraform -y
